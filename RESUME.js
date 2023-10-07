0a734d38b75d2255baa1cf045022e783114aab51
Ali-Stranger <alishaukat6292@gmail.com>
2023-10-08 01:50:01 +0500

    first commit

---
+++ RESUME.js
@@ -0,0 +1,17 @@
+//HAMBURGER
+burger=document.getElementById("burger")
+navbar = document.getElementById("navbar")
+navbarmain = document.getElementById("height")
+navbarmains = document.getElementById("img")
+
+burger.addEventListener('click',()=>{
+    
+    navbarmain.classList.toggle('h-[70px]')
+    navbarmains.classList.toggle('opacity-0')
+    navbar.classList.toggle('hidden')
+})
+
+
+//SKILLS SECTION
+
+
